4670ac1de8cef121fcf15111ee31ef92070b9c6d
Nahida4479 <szymon.cyb13@gmail.com>
2026-08-08 21:28:15 +0200

    Create AI test system and upadate README.md

--- mcbot.js
+++ mcbot.js
@@ -1,12 +1,55 @@
 const mineflayer = require('mineflayer');
 const mineflayer_pvp = require('mineflayer-pvp');
 require('dotenv').config();
-const { mineflayer: mineflayerViewer } = require("prismarine-viewer");
-const canvas = require('canvas');
 
 function date() {
     return new Date().toLocaleString()
 }
+
+// Test AI Model
+const geminiModels = ['gemini-2.0-flash', 'gemini-1.5-flash']
+const groqModels = ['llama-3.3-70b-versatile', 'llama-3.1-8b-instant']
+
+async function testGemini(model) {
+    try {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${process.env.GEMINI_API_KEY}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ contents: [{ parts: [{ text: 'text'}] }] })
+        })
+        console.log(response.ok ?`${model}: ACTIVE` : `${model}: ERROR`)
+    } catch {
+        console.log(`${model}: ERROR`)
+    }
+}
+
+async function testGroq(model) {
+    try {
+        const response = await fetch('https://api.groq.com/openai/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json',
+                'Authrization': `Bearer ${process.env.GROG_FREE_API}`
+            },
+            body: JSON.stringify({ model, messages: [{ role: 'user', content: 'test' }] })
+        })
+        console.log(response.ok ? `${model}: ACTIVE` : `${model}: ERROR`)
+    } catch {
+        console.log(`${model}: ERROR`)
+    }
+}
+
+
+async function testAllModels() {
+for (const test of geminiModels) {
+    await testGemini(test)
+}
+
+for (const test of groqModels) {
+    await testGroq(test)
+}
+}
+testAllModels();
 
 
 function createBot() {
